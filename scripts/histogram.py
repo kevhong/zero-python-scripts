db2c9493c3e1a3dd11029308b375eafeba299a44
Kevin Hong <khong@uchicago.edu>
2017-03-01 04:18:21 -0600

    cleaned up organization

--- scripts/histogram.py
+++ scripts/histogram.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 Creates Histograms of the recovery times
 '''
 
-def get_times_hist (fn, get_leaf = True):
-    times = []
 
+def get_times_hist(fn, get_leaf=True):
+    times = []
     with open(fn, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in reader:
@@ -20,7 +20,7 @@ def get_times_hist (fn, get_leaf = True):
             elif get_leaf and not has_children:
                 times.append(row[-1])
 
-    times = np.asarray(map (lambda x: np.float64(x), times))
+    times = np.asarray(map(lambda x: np.float64(x), times))
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -29,5 +29,5 @@ def get_times_hist (fn, get_leaf = True):
 
 
 if __name__ == '__main__':
-    get_times_hist (sys.argv[1])
-    get_times_hist (sys.argv[1], False)
+    get_times_hist(sys.argv[1])
+    get_times_hist(sys.argv[1], False)
